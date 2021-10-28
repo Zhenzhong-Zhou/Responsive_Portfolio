b4b2ab0ed57e2b9cb358c37617017a085692598a
Zhenzhong Zhou <bob.zhou@usask.ca>
2021-10-27 23:31:24 -0600

    Add project component and css and constants into projects component

--- src/components/Projects/Project/Project.jsx
+++ src/components/Projects/Project/Project.jsx
@@ -3,7 +3,14 @@ import "./project.css";
 const Project = () => {
 	return (
 		<div className={"project"}>
-			Project
+			<div className={"project-browser"}>
+				<div className={"project-circle"}></div>
+				<div className={"project-circle"}></div>
+				<div className={"project-circle"}></div>
+			</div>
+			<a href={""} target={"_blank"} rel={"noreferrer"}>
+				<img src={""} alt={"Project Cover"} className={"project-image"}/>
+			</a>
 		</div>
 	);
 };
---
+++ src/constants/constants.js
@@ -0,0 +1,58 @@
+export const projects = [
+	{
+		title: 'MERN Memories',
+		description: "Using React, Node.js, Express & MongoDB build a Full Stack MERN Application. The App is called Memories and it is a simple social media app that allows users to post interesting events that happened in their lives.",
+		image: '/images/memories.png',
+		tags: ['Mongo', 'Express', 'React', 'Node'],
+		source: 'https://github.com/Zhenzhong-Zhou/Memory_Project',
+		visit: 'https://timeline-memories.netlify.app',
+		id: 0,
+	},
+	{
+		title: 'E-Commerce',
+		description: "While building it I use many advanced React & JavaScript topics, and use Stripe for card transactions. And trust me, e-commerce applications are impressive.",
+		image: '/images/2.png',
+		tags: ['React', 'JavaScript'],
+		source: "https://github.com/Zhenzhong-Zhou/E-Commerce",
+		visit: "https://e-commerce-store-practice.netlify.app/",
+		id: 1,
+	},
+	{
+		title: 'WebRTC App',
+		description: "It is React Video Chat Application using WebRTC.",
+		image: '/images/3.jpg',
+		tags: ['React', 'WebRTC'],
+		source: 'https://google.com',
+		visit: 'https://google.com',
+		id: 2,
+	},
+	{
+		title: 'Unichat',
+		description: "It is a full Realtime Chat Application",
+		image: '/images/4.jpg',
+		tags: ['React', 'ChatEngine', 'Firebase'],
+		source: 'https://google.com',
+		visit: 'https://google.com',
+		id: 3,
+	},
+	{
+		title: 'Depth Of Hell',
+		description: "Depths of Hell is small boss-rush game developed by a team working (Rocket Studios) remotely from home. The Rocket Studios was created by CMPT406 group in 2021. \n" +
+			"\n" +
+			"The game features interesting boss battles combined with dynamic difficulty adjustment, thus making it easier or more difficult depending on how the player is performing. The goal is to gain a sense of progression, and insight into how the fights work. ",
+		image: '/images/depth-of-hell.png',
+		tags: ['Unity 2D', 'C#'],
+		source: 'https://github.com/Rocket-Studios/BulletHell',
+		visit: 'https://foolmonkey.github.io/DepthsOfHellWeb/',
+		id: 4,
+	},
+	{
+		title: 'Admin Dashboard',
+		description: "It is an admin panel design using React, Recharts, Material UI, and CSS. There are some widgets showing some sales information, new members and latest transactions. The analytic chart displays active users monthly.",
+		image: '/images/dashboard.png',
+		tags: ['React', 'JavaScripts', "Recharts", "Material UI", "CSS"],
+		source: 'https://github.com/Zhenzhong-Zhou/Admin_Dashboard',
+		visit: 'https://backend-dashboard.netlify.app/',
+		id: 4,
+	},
+];
