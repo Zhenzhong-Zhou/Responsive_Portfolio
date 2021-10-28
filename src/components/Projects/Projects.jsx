a4eac1f499c04b545db056ccdeda49238bf9508c
Zhenzhong Zhou <bob.zhou@usask.ca>
2021-10-27 23:08:48 -0600

    Add projects component into app

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -4,8 +4,18 @@ import "./projects.css";
 const Projects = () => {
 	return (
 		<div className={"projects"}>
-			Projects
-			<Project/>
+			<div className={"projects-texts"}>
+				<h1 className={"projects-title"}>Create & inspire.</h1>
+				<p className={"projects-desc"}>
+					Beautiful homes, stunning portfolio styles & a whole lot more awaits
+					inside.
+				</p>
+			</div>
+			<div className={"projects-list"}>
+				<Project/>
+				<Project/>
+				<Project/>
+			</div>
 		</div>
 	);
 };
